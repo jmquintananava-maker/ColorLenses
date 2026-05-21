12abe5496b92c6254e081c133977d6c80033cdd5
Jesus M.Q.N <jesus.q@MacBook-Pro-de-Jesus.local>
2026-05-21 14:11:06 -0600

    Primer proyecto ColorLenses

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "ColorLenses backend funcionando 🚀",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en puerto 3000");
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        background: "#0f172a",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1 style={{ fontSize: "4rem", margin: 0 }}>
+        ColorLenses
+      </h1>
+
+      <p style={{ opacity: 0.7 }}>
+        Proyecto React funcionando 🚀
+      </p>
+    </div>
+  );
+}
+
+export default App;
